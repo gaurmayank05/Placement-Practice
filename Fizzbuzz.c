33c693ff18014a34040038304631c48079bcef34
Mayank Gaur <gaurmayank05@gmail.com>
2023-08-30 22:37:09 +0530

    Add files via upload

---
+++ Fizzbuzz.c
@@ -0,0 +1,31 @@
+/*
+    Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz"
+    instead of the number, and for the multiple of five ,print "Buzz". For numbers which are multiples
+    of both three and five, print "FizzBuzz".
+ */
+
+#include <stdio.h>
+
+void main()
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+}
